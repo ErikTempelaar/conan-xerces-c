fdb7e2dd17a752babd006b6a2b96dff6a02e44ad
Erik Tempelaar <eriktempelaar@altran.com>
2016-05-26 21:52:41 +0200

    Build only x86_64 configurations, compiling x86 is not yet supported.

--- build.py
+++ build.py
@@ -3,4 +3,11 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="admin")
     builder.add_common_builds()
+
+    filtered_builds = []
+    for settings, options in builder.builds:
+        if settings["arch"] == "x86_64":
+             filtered_builds.append([settings, options])
+    builder.builds = filtered_builds
+
     builder.run()
